8052e9881cbff34f68e932335de4446449b09a8d
mechim.cook20@gmail.com <mechim.cook20@gmail.com>
2025-06-12 20:48:04 -0500

    Fix schema

--- lib/face_checkin/schema/profile.ex
+++ lib/face_checkin/schema/profile.ex
@@ -5,14 +5,14 @@ defmodule FaceCheckin.Profile do
   schema "profiles" do
     field :name, :string
     field :profile_pic, :binary
-    field :status, :string, default: "out"
+    field :checked_in, :boolean
     has_many :profiles_faces, FaceCheckin.ProfileFace
     timestamps()
   end
 
   def changeset(profile, attrs) do
     profile
-    |> cast(attrs, [:name, :profile_pic, :status])
+    |> cast(attrs, [:name, :profile_pic, :checked_in])
     |> validate_required([:name])
   end
 end
